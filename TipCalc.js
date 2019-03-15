3586c4bbbe8e412700d6115d75a4060c789eb7eb
Stephen Gracia <32041710+phteven7@users.noreply.github.com>
2019-03-14 19:47:01 -0700

    final desing

--- TipCalc.js
+++ TipCalc.js
@@ -2,15 +2,16 @@ function taxCalc() {
     //Implement Conversion of use input to decimal.
     var bill = Number(document.getElementById("bill").value);
     var tip = Number(document.getElementById("tip").value);
-    
+    var split = Number(document.getElementById("split").value);
+    //Converts tip to percentage
+    var tipPercentage = tip / 100;
     //the unary operator "+" converts decTotal to a number without it is a string//
-    var tipTotal = +(bill * tip).toFixed(2);
-    var billTotal = bill + tipTotal;
+    var totalTip = +(bill * tipPercentage).toFixed(2);
+    var totalBill = (bill + totalTip).toFixed(2);
+    var perPerson = (totalBill / split).toFixed(2);
 
-    document.getElementById("tipDisplay").innerHTML = tipTotal;
-    document.getElementById("totalDisplay").innerHTML = billTotal;
+    document.getElementById("totalTip").innerHTML = "$" + totalTip;
+    document.getElementById("totalBill").innerHTML = "$" + totalBill;
+    document.getElementById("perPerson").innerHTML = "$" + perPerson;
 }
 
-function connected() {
-    console.log("hello");
-}
